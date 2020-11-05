c8484e340a37ade2dc63806e95e601766a2c8850
Redzic18 <73280790+Redzic18@users.noreply.github.com>
2020-11-05 11:33:41 +0100

    Update calculator.c
    den int: nmr2 habe ich mit einen float ausgetauscht
    die Operatoren - , * und / hinzugefügt mit switch case.
     case '-':
            printf("%.1lf - %.1lf = %.1lf",num1, num2, num1 - num2);
                break;
            case '*':
            printf("%.1lf * %.1lf = %.1lf",num1, num2, num1 * num2);
                break;
            case '/':
                printf("%.1lf / %.1lf = %.1lf",num1, num2, num1 / num2);

--- calculator.c
+++ calculator.c
@@ -6,26 +6,35 @@
 int main(){
 
 	float num1;
-	int num2;
-	float ans;
-	char op;
+	float num2;
+	float x;
+	char operator;
 	printf("\nEnter an operator (+,-,/,*): \n");
-	scanf("%c",&op);
+	scanf("%c",&operator);
 
 	printf("\nEnter two numbers: ");
 	scanf("%f",&num1);
 	scanf("%f",&num2);
-	
-	switch(op){
-		case '+' : 
-			ans = num1 + num2;
+
+	switch(operator){
+		case '+' :
+			x = num1 + num2;
 			break;
 		default:
 			printf("Enter a correct operator!");
+        case '-':
+        printf("%.1lf - %.1lf = %.1lf",num1, num2, num1 - num2);
+            break;
+        case '*':
+        printf("%.1lf * %.1lf = %.1lf",num1, num2, num1 * num2);
+            break;
+        case '/':
+            printf("%.1lf / %.1lf = %.1lf",num1, num2, num1 / num2);
+        break;
 			return 1;
-	
+
 	}
 	printf("\n The result is as follows:\n");
-	printf("%f %c %f = %f",num1, op ,num2,ans);
+	printf("%f %c %f = %f",num1, op ,num2,x);
 	return 0;
 }
